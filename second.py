1515afb44b51d860900c6b719f8bb91060723e02
Tamirlan Kadyrbekov <64827939+Tamirlan-dev@users.noreply.github.com>
2022-11-29 12:33:47 +0600

    Update second.py

--- second.py
+++ second.py
@@ -9,20 +9,19 @@ for images in os.listdir(folder_dir):
     if (images.endswith(".png") or images.endswith(".jpg")\
         or images.endswith(".jpeg")):
         downloadpath=r'D:\\researsh\\images\\'+ images
-
         savepath=r'D:\\researsh\\test\\'+ images
         img = cv2.imread(downloadpath)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (0,0), sigmaX=33, sigmaY=33)
         divide = cv2.divide(gray, blur, scale=255)
         thresh = cv2.threshold(divide, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
-        cv2.imwrite(r'D:\researsh\test\binarization'+images,thresh)
+        #cv2.imwrite(r'D:\researsh\test\binarization'+images,thresh)
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
         morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-        cv2.imwrite(r'D:\researsh\test\remove_noise'+images,morph)
+        #cv2.imwrite(r'D:\researsh\test\remove_noise'+images,morph)
         kernel = np.ones((5,5),np.uint8)
         skel = cv2.erode(morph,kernel,iterations = 5)
-        cv2.imwrite(r'D:\researsh\test\thinning'+images,skel)
+        #cv2.imwrite(r'D:\researsh\test\thinning'+images,skel)
         cv2.imwrite(savepath, skel)
 
 
